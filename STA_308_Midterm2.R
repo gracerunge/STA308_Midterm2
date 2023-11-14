c69377c84b32e1df427714316dc14370c45d5816
gracerunge <rungegn@miamioh.edu>
2023-11-14 18:21:52 -0500

    created project and script

---
+++ STA_308_Midterm2.R
@@ -0,0 +1,9 @@
+#############################
+#* STA 308 Fall 2023 Midterm 2 
+#* Author: Grace Runge 
+#* Date: 11/14/2023
+#* Purpose: To write script which 
+#*    answers the required questions
+#*    for our second midterm. 
+
+
